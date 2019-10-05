6de05f0c24ed4d2acdad2933306605d2f3e3bbc4
LethalMaus <consolereviver@gmail.com>
2019-10-05 21:35:36 +0200

    testing embedded reactions

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,7 @@
+/*
 function removeWatermark() {
 	var length = document.getElementsByTagName("a").length;
 	document.getElementsByTagName("a")[length-5].style.display = "none";
 }
 window.onload=setTimeout(removeWatermark, 1000);
+*/
