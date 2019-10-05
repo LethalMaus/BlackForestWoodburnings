5a897d8defd3c2606ea3aeb3a982ec5a1140c7d4
LethalMaus <consolereviver@gmail.com>
2019-10-05 02:58:27 +0200

    Temporary gallery

---
+++ assets/js/scripts.js
@@ -0,0 +1,5 @@
+function removeWatermark() {
+	var length = document.getElementsByTagName("a").length;
+	document.getElementsByTagName("a")[length-5].style.display = "none";
+}
+window.onload=setTimeout(removeWatermark, 1000);
