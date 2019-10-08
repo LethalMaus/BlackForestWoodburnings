206c833b46e1b55998362e8d51194ad00e3ce376
LethalMaus <consolereviver@gmail.com>
2019-10-08 19:34:58 +0200

    Templating post

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,18 +1,16 @@
-/*
-function removeWatermark() {
-	var length = document.getElementsByTagName("a").length;
-	document.getElementsByTagName("a")[length-5].style.display = "none";
-}
-window.onload=setTimeout(removeWatermark, 1000);
-*/
-
 let xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://www.instagram.com/graphql/query/?query_hash=865589822932d1b43dfe312121dd353a&variables=%7B%22shortcode%22%3A%22B2dwkOSo9Qi%22%2C%22child_comment_count%22%3A3%2C%22fetch_comment_count%22%3A40%2C%22parent_comment_count%22%3A24%2C%22has_threaded_comments%22%3Atrue%7D');
 xhr.send();
 xhr.onload = function() {
-  if (xhr.status != 200) { // analyze HTTP status of the response
-    alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
-  } else { // show the result
-    alert(`Done, got ${xhr.response}`); // responseText is the server
+  if (xhr.status != 200) { 
+    console.log(`Error ${xhr.status}: ${xhr.statusText}`);
+  } else { 
+    console.log(`Done, got ${xhr.response}`);
+	var likes = "<div class='instagram-likes'>" + xhr.response.data.shortcode_media.edge_media_preview_like.count + "</div>";
+	var comments = "<div class='instagram-comments'>" + xhr.response.data.shortcode_media.edge_media_preview_comment.count + "</div>";
+	var caption = "<div class='instagram-caption'>" + xhr.response.data.shortcode_media.edge_media_to_caption.edges[0].node.text + "</div>";
+	var image = "<img class='instagram-image' src='" + xhr.response.data.shortcode_media.display_url + "'>";
+	var instagramPost = "<div class='instagram-post'>" + likes + comments + caption + image + "</div>";
+	document.getElementById("instagram-gallery").innerHTML = instagramPost;
   }
 };
