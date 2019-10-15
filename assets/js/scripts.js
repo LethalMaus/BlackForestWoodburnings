45ad7a1d5078f232e98cb73fc5283b685f9e1325
LethalMaus <consolereviver@gmail.com>
2019-10-15 18:04:47 +0200

    Posts JSON

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,20 +1,48 @@
 let xhr = new XMLHttpRequest();
-/*
-Query can be kept, only the post name needs to be swapped
-*/
-var postName = 'B3e_NzkoRGe';
-xhr.open('GET', 'https://www.instagram.com/graphql/query/?query_hash=865589822932d1b43dfe312121dd353a&variables=%7B%22shortcode%22%3A%22' + postName + '%22%7D');
-xhr.send();
-xhr.onload = function() {
-  if (xhr.status != 200) { 
-    console.log(`Error ${xhr.status}: ${xhr.statusText}`);
-  } else { 
-	var response = JSON.parse(xhr.responseText);
-	var likes = "<div class='instagram-likes'>" + response.data.shortcode_media.edge_media_preview_like.count + "</div>";
-	var comments = "<div class='instagram-comments'>" + response.data.shortcode_media.edge_media_preview_comment.count + "</div>";
-	var caption = "<div class='instagram-caption'>" + response.data.shortcode_media.edge_media_to_caption.edges[0].node.text + "</div>";
-	var image = "<img class='instagram-image' src='" + response.data.shortcode_media.display_url + "'>";
-	var instagramPost = "<div class='instagram-post'>" + likes + comments + caption + image + "</div>";
-	document.getElementById("instagram-gallery").innerHTML = instagramPost;
-  }
-};
+
+function loadPosts() {
+	xhr.open('GET', 'posts.json');
+	xhr.send();
+	xhr.onload = function() {
+		if (xhr.status != 200) { 
+			console.log(`Error ${xhr.status}: ${xhr.statusText}`);
+		} else { 
+			var posts = JSON.parse(xhr.responseText);
+			posts.forEach(loadPost);
+		}
+	}
+}
+
+function loadPost(postId) {
+	xhr.open('GET', 'https://www.instagram.com/graphql/query/?query_hash=865589822932d1b43dfe312121dd353a&variables=%7B%22shortcode%22%3A%22' + postId + '%22%7D');
+	xhr.send();
+	xhr.onload = function() {
+		if (xhr.status != 200) { 
+			console.log(`Error ${xhr.status}: ${xhr.statusText}`);
+		} else { 
+			var response = JSON.parse(xhr.responseText);
+			
+			var likeCount;
+			if (response.data.shortcode_media.edge_media_preview_like && response.data.shortcode_media.edge_media_preview_like.count) {
+				likeCount = response.data.shortcode_media.edge_media_preview_like.count;
+			} else {
+				likeCount = 0;
+			}
+			var likes = "<div class='instagram-likes'>" + likeCount + "</div>";
+			
+			var commentCount;
+			if (response.data.shortcode_media.edge_media_preview_comment && response.data.shortcode_media.edge_media_preview_comment.count) {
+				commentCount = response.data.shortcode_media.edge_media_preview_comment.count;
+			} else {
+				commentCount = 0;
+			}
+			var comments = "<div class='instagram-comments'>" + commentCount + "</div>";
+			var caption = "<div class='instagram-caption'>" + response.data.shortcode_media.edge_media_to_caption.edges[0].node.text + "</div>";
+			var image = "<img class='instagram-image' src='" + response.data.shortcode_media.display_url + "'>";
+			var instagramPost = "<div class='instagram-post'>" + likes + comments + caption + image + "</div>";
+			document.getElementById("instagram-gallery").innerHTML += instagramPost;
+		}
+	};
+}
+
+loadPosts();
