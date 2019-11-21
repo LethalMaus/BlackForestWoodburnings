0f3a305eac9576799855d2f1a961222580a3afcd
LethalMaus <consolereviver@gmail.com>
2019-11-22 00:22:35 +0100

    Show failed executed posts

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -49,47 +49,46 @@ function loadPostAndReplace(postId, currentPostShown) {
 	xhr.onload = async function() {
 		if (xhr.status != 200) { 
 			console.log(`Error ${xhr.status}: ${xhr.statusText}`);
-		} else { 
-			var response = JSON.parse(xhr.responseText);
-			
-			var likeCount;
-			if (response.data.shortcode_media.edge_media_preview_like 
-			&& response.data.shortcode_media.edge_media_preview_like.count) {
-				likeCount = response.data.shortcode_media.edge_media_preview_like.count;
-			} else {
-				likeCount = 0;
-			}
-			var likes = "<div class='instagram-likes'><img class='instagram-symbols' src='images/heart.png' alt='Likes'> " + likeCount + "</div>";
-			
-			var commentCount;
-			if (response.data.shortcode_media.edge_media_to_comment 
-			&& response.data.shortcode_media.edge_media_to_comment.count) {
-				commentCount = response.data.shortcode_media.edge_media_to_comment.count;
-			} else {
-				commentCount = 0;
-			}
-			var comments = "<div class='instagram-comments'><img class='instagram-symbols' src='images/comment.png' alt='Comments'> " + commentCount + "</div>";
-			var captionText;
-			if (response.data.shortcode_media.edge_media_to_caption 
-			&& response.data.shortcode_media.edge_media_to_caption.edges[0]
-			&& response.data.shortcode_media.edge_media_to_caption.edges[0].node 
-			&& response.data.shortcode_media.edge_media_to_caption.edges[0].node.text) {
-				captionText = response.data.shortcode_media.edge_media_to_caption.edges[0].node.text;
-			} else {
-				captionText = "";
-			}
-			var caption = "<div class='instagram-caption'>" + captionText + "</div>";
-			var image = "<img id='image-" + postId + "' class='instagram-image-invisible' src='" + response.data.shortcode_media.display_url + "'>";
-			var instagramPost = "<div id='" + postId + "' onclick='openPost(this.id)' class='instagram-post-invisible'>" + likes + comments + caption + image + "</div>";
-			document.getElementById("instagram-gallery").innerHTML += instagramPost;
-			await timeout(1000);
-			document.getElementById(postId).className = "instagram-post";
-			await timeout(3000);
-			document.getElementById('image-' + postId).className = "instagram-image";
-			if (currentPostShown) {
-				document.getElementById(currentPostShown).outerHTML = "";
-			}
 		}
+		var response = JSON.parse(xhr.responseText);
+		var likeCount;
+		if (response.data.shortcode_media.edge_media_preview_like 
+		&& response.data.shortcode_media.edge_media_preview_like.count) {
+			likeCount = response.data.shortcode_media.edge_media_preview_like.count;
+		} else {
+			likeCount = 0;
+		}
+		var likes = "<div class='instagram-likes'><img class='instagram-symbols' src='images/heart.png' alt='Likes'> " + likeCount + "</div>";
+		
+		var commentCount;
+		if (response.data.shortcode_media.edge_media_to_comment 
+		&& response.data.shortcode_media.edge_media_to_comment.count) {
+			commentCount = response.data.shortcode_media.edge_media_to_comment.count;
+		} else {
+			commentCount = 0;
+		}
+		var comments = "<div class='instagram-comments'><img class='instagram-symbols' src='images/comment.png' alt='Comments'> " + commentCount + "</div>";
+		var captionText;
+		if (response.data.shortcode_media.edge_media_to_caption 
+		&& response.data.shortcode_media.edge_media_to_caption.edges[0]
+		&& response.data.shortcode_media.edge_media_to_caption.edges[0].node 
+		&& response.data.shortcode_media.edge_media_to_caption.edges[0].node.text) {
+			captionText = response.data.shortcode_media.edge_media_to_caption.edges[0].node.text;
+		} else {
+			captionText = "";
+		}
+		var caption = "<div class='instagram-caption'>" + captionText + "</div>";
+		var image = "<img id='image-" + postId + "' class='instagram-image-invisible' src='" + response.data.shortcode_media.display_url + "'>";
+		var instagramPost = "<div id='" + postId + "' onclick='openPost(this.id)' class='instagram-post-invisible'>" + likes + comments + caption + image + "</div>";
+		document.getElementById("instagram-gallery").innerHTML += instagramPost;
+		await timeout(1000);
+		document.getElementById(postId).className = "instagram-post";
+		await timeout(3000);
+		document.getElementById('image-' + postId).className = "instagram-image";
+		if (currentPostShown) {
+			document.getElementById(currentPostShown).outerHTML = "";
+		}
+		
 	};
 }
 
